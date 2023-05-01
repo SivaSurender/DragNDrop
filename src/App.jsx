4558b0398894e58dd557846c3daa75fac89452c7
SivaSurender <e.surender14@gmail.com>
2023-05-01 16:27:15 +0530

    added code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import DragDrop from "./components/DragDrop";
 import "./App.css";
 
 function App() {
@@ -8,11 +5,7 @@ function App() {
   1. wrap the whole app which needs access to draggable features with DndProvider, similar to contedt provider
   2. provder will always need to be provided with a backend ie HTML5backend
   */
-  return (
-    <DndProvider backend={HTML5Backend}>
-      <DragDrop />
-    </DndProvider>
-  );
+  return <p>Test</p>;
 }
 
 export default App;
